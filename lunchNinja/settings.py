11ac67bbb60acf93787bf2eabea81b1795e7b8b6
YixinHu0321 <yh3244@nyu.edu>
2019-11-05 15:53:30 -0500

    change database setting

--- lunchNinja/settings.py
+++ lunchNinja/settings.py
@@ -78,26 +78,24 @@ WSGI_APPLICATION = "lunchNinja.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        "TEST": {"NAME": os.path.join(BASE_DIR, "db.sqlite3")},
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#         "TEST": {"NAME": os.path.join(BASE_DIR, "db.sqlite3")},
+#     }
+# }
 #
 # TEST_RUNNER = "homepage.utils.UnManagedModelTestRunner"
 #
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "lunchninja",
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#         # "USER": "postgres",
-#         # "PASSWORD": "password",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "lunchninja",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
 
 
 # Password validation
